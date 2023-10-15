17ef3490df54e8ece109c979094df5814faaeeb5
sarwar-asik <sarwarasik@gmail.com>
2023-10-15 13:23:06 +0600

    service validation

---
+++ prisma/migrations/20231015072011_review/migration.sql
@@ -0,0 +1,21 @@
+-- AlterTable
+ALTER TABLE "Service" ADD COLUMN     "serviceDate" TIMESTAMP(3);
+
+-- CreateTable
+CREATE TABLE "Review" (
+    "id" TEXT NOT NULL,
+    "details" TEXT NOT NULL,
+    "rating" INTEGER,
+    "serviceId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Review_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_serviceId_fkey" FOREIGN KEY ("serviceId") REFERENCES "Service"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
