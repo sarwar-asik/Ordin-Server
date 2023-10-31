27cb925c0bc0373836751e0565df89fb31f067fc
sarwar-asik <sarwarasik@gmail.com>
2023-10-31 11:25:59 +0600

    payment router done

--- src/app/modules/Payment/Payment.route.ts
+++ src/app/modules/Payment/Payment.route.ts
@@ -1,10 +1,28 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Router } from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import { PaymentController } from './Payment.controller';
 const router = Router();
 router.get('/');
 router.post('/', PaymentController.initPayment);
 router.post('/webhook', PaymentController.webHook);
+
+router.get(
+  '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  PaymentController.getAllPayment
+);
+
+router.get(
+  '/userPayment',
+  auth(ENUM_USER_ROLE.USER),
+  PaymentController.getUserAllPayment
+);
+
+router.get('/:serviceId', PaymentController.getUserPaymentByService);
+
+router.get('/:id', PaymentController.getSingleDataById);
 
 export const paymentRoutes = router;
